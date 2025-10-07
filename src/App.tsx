dcc291fa71f1f7ca4d548763e9329b1cfd783f3b
pabloc4-hue <pabloc4@illinois.edu>
2025-10-06 21:58:00 -0500

    second try

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-// src/App.tsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ListView from './pages/ListView';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/mp2">
       <Routes>
         <Route path="/" element={<ListView />} />
       </Routes>
